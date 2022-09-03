abff71370d4f25a5665e9a95b3fd6fcb9bab534d
NatZuKix <nutthanon.natzuki@mail.kmutt.ac.th>
2022-09-04 03:49:58 +0700

    sent4

--- minMedMax.js
+++ minMedMax.js
@@ -2,26 +2,26 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
  let a=[n1,n2,n3];
- if(n1<n3){
-  if(n1<n2){
+ if(n1<n3 && n1<n2){
     if(n2<n3){
       a=[n1,n2,n3]; 
     }else{
       a=[n1,n3,n2];
     }
+}else if(n2<n1 && n2<n3){
+    if(n1<n3){
+      a=[n2,n1,n3]; 
+    }else{
+      a=[n2,n3,n1];
+  }
+}else{
+  if(n2<n1){
+    a=[n3,n2,n1]; 
   }else{
-    a=[n2,n1,n3];
-  }
- }else{
-  if(n3<n2){
-    a=[n3,n2,n1];
-  }else{
-    a=[n2,n3,n1];
-  }
-  if(n3<n1 && n3<n2 && n2<n1){
-    a=[n3,n1,n2];
-  }
- }
+    a=[n3,n1,n3];
+}
+}
+
  return {min: a[0], mid: a[1], max: a[2]};
 
 
